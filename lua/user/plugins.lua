ab3bd98a3554681ee74f2a6427f8bbdf9f6ad0a8
Henry Derbyshire <henry@htderbyshire.com>
2022-03-15 19:48:19 +0000

    fixed lsp and added formatting and linting with prettier and eslint

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -78,9 +78,8 @@ return packer.startup(function(use)
   -- LSP
   use { "neovim/nvim-lspconfig" } -- enable LSP
   use { "williamboman/nvim-lsp-installer" } -- simple to use language server installer
-
-  -- Formatting
-  use { "jose-elias-alvarez/null-ls.nvim" }
+  use { "tamago324/nlsp-settings.nvim"}
+  use { "jose-elias-alvarez/null-ls.nvim" } -- for formatters and linters
 
   -- Git
   use { "lewis6991/gitsigns.nvim" }

1a3d97fffdae62001830edf00945f47f3cb67481
Henry Derbyshire <henry@htderbyshire.com>
2022-03-15 17:48:40 +0000

    removed formatting

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -1,7 +1,4 @@
--- Null ls
-local null_ls = require("null-ls")
 local M = {}
-
 
 M.setup = function()
   local signs = {
@@ -76,7 +73,7 @@ local function lsp_keymaps(bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-  -- vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
+  vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
 end
 
 M.on_attach = function(client, bufnr)
@@ -84,13 +81,6 @@ M.on_attach = function(client, bufnr)
     client.resolved_capabilities.document_formatting = false
     client.resolved_capabilities.document_range_formatting = false
   end
-
-  -- Null ls
-  vim.cmd("command! LspFormatting lua vim.lsp.buf.formatting()")
-  if client.resolved_capabilities.document_formatting then
-      vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
-  end
-
 
   lsp_keymaps(bufnr)
   lsp_highlight_document(client)
@@ -102,17 +92,6 @@ local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not status_ok then
   return
 end
-
-
--- Null ls
-null_ls.setup({
-    sources = {
-        null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.code_actions.eslint,
-        null_ls.builtins.formatting.prettierd,
-    },
-    on_attach = M.on_attach,
-})
 
 M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
 
